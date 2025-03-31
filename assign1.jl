cc91990e3b11d07f7ce287586b508db2955904ff
John Emanuelsson <john.3manuelsson@gmail.com>
2025-03-31 14:36:35 +0200

    .

---
+++ assign1.jl
@@ -0,0 +1,12 @@
+
+## Biofuels supply chain
+# 0.9l unrefined biodisel <- 1l vegtable oil + 0.2l methanol
+# 
+price_methanol = 1.5 # (Euro per liter)
+price_petrol_disel = 1 # (Euro per liter)
+max_petrol_disel = 150_000 # Liters of available petrol disel
+
+# TODO: What ratio is petrol disel to we need for refined biodisel???
+
+## Crops
+crop_area = 1_600 # ha
