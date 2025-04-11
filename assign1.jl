da0d369b025d9d5354cbc710cba9648e28b32565
John Emanuelsson <john.3manauelsson@gmail.com>
2025-04-11 15:32:21 +0200

    Calculations refactored and units checked

--- assign1.jl
+++ assign1.jl
@@ -28,8 +28,8 @@
 ## Optimize: profit from biodiesel production
 # profit = sum of each each product: amount sold * (product_price - product_tax) - production cost
 ## Variables:
-# x -- crop area???
-# y -- product liters???
+# x -- crop area (ha)
+# y -- product liters (l)
 
 
 ### Data:
@@ -41,52 +41,69 @@
 petrol_diesel_limit = 150_000 # Liters of available petrol disel
 ## price (Euro per liter)
 # methanol, petrol diesel
-# fuel_prices = [1.5, 1.0]
-price_methanol = 1.5 # (Euro per liter)
-price_petrol_diesel = 1 # (Euro per liter)
+price_methanol = 1.5 # (Euro/l)
+price_petrol_diesel = 1 # (Euro/l)
 
 
 ## Crops
 # soybeans, sunflower seeds, cotton seeds
-crop_area = 1_600 # ha
-crop_water_limit = 5_000 # Mli
+crop_total_area = 1_600 # ha
+crop_total_water_limit = 5_000 # Ml
 crop_count = 3
-crop_yields = 1000.0 .* [2.6, 1.4, 0.9] # l/ha
+crop_yields = 1000.0 .* [2.6, 1.4, 0.9] # 1000 * t/ha = kg/ha
 crop_water_demands = [5.0, 4.2, 1.0] # Ml/ha
 crop_oil_contents = [0.178, 0.216, 0.433] # l/kg
 
 ## Final products: B5, B30, B100
-prod_demand = 280_000
+prod_total_demand = 280_000 # l
 prod_count = 3
 prod_biodiesel_ratios = [0.05, 0.3, 1.0]
 prod_petrol_diesel_ratios = 1.0 .- prod_biodiesel_ratios
-prod_prices = [1.43, 1.29, 1.16]
+prod_prices = [1.43, 1.29, 1.16] # Euro/l
 prod_taxes = [0.20, 0.05, 0]
 
 
 using JuMP, Clp
 
 model = Model(Clp.Optimizer)
-@variable(model, x[1:crop_count], lower_bound=0)
-@variable(model, y[1:prod_count], lower_bound=0)
+@variable(model, x[1:crop_count], lower_bound=0) # ha
+@variable(model, y[1:prod_count], lower_bound=0) # l
+
+water_usage         = x .* crop_water_demands # ha*(Ml/ha) = Ml 
+
+# Extraction of vegtable oils from seeds
+# units: ha * (kg/ha) * (l/kg) = l 
+vegtable_oils       = x .* crop_yields .* crop_oil_contents # l 
+
+# 0.9l biodiesel <- 1l veetable oil, 0.2l methanol
+biodiesel_produced  = 0.9 .*vegtable_oils  # l
+methanol_usage      = 0.2 .* vegtable_oils # l
+
+# Each product is a mix of biodiesel and petrol diesel
+biodiesel_usage     = y .* prod_biodiesel_ratios # l
+petrol_diesel_usage = y .* prod_petrol_diesel_ratios # l
+@assert all(isapprox.(prod_biodiesel_ratios .+ prod_petrol_diesel_ratios, 1.0)) # They must add up to 1
 
 # Crop constraints
-@constraint(model, sum(x) <= crop_area)
-@constraint(model, sum(x .* crop_water_demands) <= crop_water_limit)
-
+@constraint(model, sum(x) <= crop_total_area) # ha <= ha
+@constraint(model, sum(water_usage) <= crop_total_water_limit) # Ml <= Ml
 # Product constraints
-@constraint(model, sum(y .* prod_petrol_diesel_ratios) <= petrol_diesel_limit)
-@constraint(model, sum(y) >= prod_demand)
-
+@constraint(model, sum(y) >= prod_total_demand) # l >= l
+@constraint(model, sum(petrol_diesel_usage) <= petrol_diesel_limit) # l <= l
 # Constraint between x and y: We can only make so much of each product from the biodiesel
-@constraint(model, sum(y .* prod_biodiesel_ratios) <= sum(x .* crop_yields .* crop_oil_contents .* 0.9))
+@constraint(model, sum(biodiesel_usage) <= sum(biodiesel_produced)) # l <= l
 
-sold = sum(y .* (prod_prices .* (1 .- prod_taxes)))
-cost_methanol = price_methanol * 0.2 * sum(x .* crop_yields .* crop_oil_contents)
-cost_petrol_diesel = price_petrol_diesel .* sum(y .* prod_petrol_diesel_ratios) 
-cost = cost_methanol + cost_petrol_diesel
 
-@objective(model, Max, sold - cost)
+total_cost_methanol = sum(price_methanol .* methanol_usage) # (Euro/l) * l = Euro
+total_cost_petrol_diesel = sum(price_petrol_diesel .* petrol_diesel_usage) # (Euro/l) * l = Euro
+total_cost = total_cost_methanol + total_cost_petrol_diesel # Euro + Euro = Euro
+
+revenues = y .* prod_prices # l * (Euro/l) = Euro
+total_revenue = sum(revenues) # Euro
+taxes = revenues .* prod_taxes # Euro
+total_tax = sum(taxes) # Euro
+
+@objective(model, Max, total_revenue - total_tax - total_cost) # Euro - Euro - Euro = Euro
 optimize!(model)
 
 println("Termination status: ", termination_status(model))
